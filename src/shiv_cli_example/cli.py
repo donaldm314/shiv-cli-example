f56396e939897e7d941e57c2be428d0129f948a9
Don Murray <dmurray@dmurray.ca>
2024-07-19 15:17:16 +0000

    --help and -h

--- src/shiv_cli_example/cli.py
+++ src/shiv_cli_example/cli.py
@@ -1,8 +1,10 @@
 import click
 from .main import main as run_main
 
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-@click.command()
+
+@click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument('name')
 def main(name):
     """A simple CLI."""
