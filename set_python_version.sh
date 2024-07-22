ae58a2f556af76e726e5297c9c681874fb1e0649
Don Murray <dmurray@dmurray.ca>
2024-07-22 14:35:57 +0000

    Moved to set_python_version.sh

---
+++ set_python_version.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+exit_code=0
+if [ -z ${MY_PYTHON_VERSION} ]; then
+    echo "ERROR: MY_PYTHON_VERSION must be set!"
+    exit_code=1
+fi
+
+if [ 0 -ne $exit_code ]; then
+    exit $exit_code
+fi
+
+
+set -e
+DOCKERFILE=Dockerfile
+PIPFILE=Pipfile
+GITHUB_ACTIONS='.github/workflows/github-actions.yml'
+
+
+write_dockerfile() {
+    cat > ${DOCKERFILE} <<EOF
+FROM mcr.microsoft.com/devcontainers/python:1-${MY_PYTHON_VERSION}-bookworm
+EOF
+}
+
+update_pipfile() {
+    sed -i -r -e "s,^python_version = \".*\",python_version = \"${MY_PYTHON_VERSION}\"," ${PIPFILE}
+}
+
+update_github_actions() {
+    devcontainer_prefix='mcr.microsoft.com/devcontainers/python:1-'
+    sed -i -r -e "s,([ ]+container.[ ]+).*,\1${devcontainer_prefix}${MY_PYTHON_VERSION}-bookworm," ${GITHUB_ACTIONS}
+}
+
+write_dockerfile
+update_pipfile
+update_github_actions
